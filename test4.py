0c415eed64fd562de2623c0c0ffd84f7d7c47654
Chintan Raiyani <chintan.raiyani@sap.com>
2019-04-01 15:20:21 +0530

    test4 block

--- test4.py
+++ test4.py
@@ -2,8 +2,32 @@ from client import Client
 from transaction import Transaction
 from block import Block
 
+def display_transaction(transaction):
+    #for transaction in transactions:
+    dict = transaction.to_dict()
+    print ("sender: " + dict['sender'])
+    print ('-----')
+    print ("recipient: " + dict['recipient'])
+    print ('-----')
+    print ("value: " + str(dict['value']))
+    print ('-----')
+    print ("time: " + str(dict['time']))
+    print ('-----')
+    
+def dump_blockchain (self):
+    print ("Number of blocks in the chain: " + str(len (self)))
+    for x in range (len(TPCoins)):
+        block_temp = TPCoins[x]
+        print ("block # " + str(x))
+        for transaction in block_temp.verified_transactions:
+            display_transaction (transaction)
+            print ('--------------')
+        print ('=====================================')
+
 if __name__ == "__main__":
     
+    TPCoins = []
+
     Dinesh = Client()
 
     t0 = Transaction (
@@ -21,3 +45,7 @@ if __name__ == "__main__":
 
     digest = hash (block0)
     last_block_hash = digest
+
+    TPCoins.append (block0)
+
+    dump_blockchain(TPCoins)
