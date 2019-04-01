a84675fd824a24f1f9fd53f64803c54170d617f6
Chintan Raiyani <chintan.raiyani@sap.com>
2019-04-01 14:34:34 +0530

    Init and create test1

---
+++ client.py
@@ -0,0 +1,30 @@
+# import libraries
+import hashlib
+import random
+import string
+import json
+import binascii
+import numpy as np
+import pandas as pd
+import pylab as pl
+import logging
+import datetime
+import collections
+
+# following imports are required by PKI
+import Crypto
+import Crypto.Random
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+
+class Client:
+   def __init__(self):
+      random = Crypto.Random.new().read
+      self._private_key = RSA.generate(1024, random)
+      self._public_key = self._private_key.publickey()
+      self._signer = PKCS1_v1_5.new(self._private_key)
+
+   @property
+   def identity(self):
+      return binascii.hexlify(self._public_key.exportKey(format='DER')).decode('ascii')
---
+++ transaction.py
@@ -0,0 +1,31 @@
+# following imports are required by PKI
+import Crypto
+import Crypto.Random
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+
+class Transaction:
+    def __init__(self, sender, recipient, value):
+        self.sender = sender
+        self.recipient = recipient
+        self.value = value
+        self.time = datetime.datetime.now()
+
+    def to_dict(self):
+        if self.sender == "Genesis":
+            identity = "Genesis"
+        else:
+            identity = self.sender.identity
+
+        return collections.OrderedDict({
+            'sender': identity,
+            'recipient': self.recipient,
+            'value': self.value,
+            'time' : self.time})
+    
+    def sign_transaction(self):
+        private_key = self.sender._private_key
+        signer = PKCS1_v1_5.new(private_key)
+        h = SHA.new(str(self.to_dict()).encode('utf8'))
+        return binascii.hexlify(signer.sign(h)).decode('ascii')
